82b5b5541c0ee81d50b97e46c472414376a39621
Fanuelberhane <fanuelberhanek@gmail.com>
2024-11-12 22:33:30 +0300

    Add files via upload

---
+++ assignment5.js
@@ -0,0 +1,27 @@
+
+
+
+
+const add = (a, b) => a + b;
+
+console.log(add);
+
+
+console.log(add (5,6));
+
+
+const multiply=(a, b) => a * b ;
+
+console.log(multiply);
+
+console.log(multiply(8, 9));
+
+
+
+let name1 = "fanuel";
+
+
+const greet=(name1) => `hello ${name1}`;
+
+console.log(greet);
+console.log(greet(name1));
